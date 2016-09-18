6c10bbe52e1fe4b43d9ea6ea3a49849f3d4cbc43
Daniel Rollings <danielrollings@Daniel-Rollingss-MacBookPro.local>
2016-09-18 22:28:30 +0100

    4.4 added Order.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160812125354) do
+ActiveRecord::Schema.define(version: 20160918201449) do
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "users_id"
+    t.integer "product_id"
+    t.float   "total"
+    t.index ["product_id"], name: "index_orders_on_product_id"
+    t.index ["users_id"], name: "index_orders_on_users_id"
+  end
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
