db0ab6e59c13fbf31f38d0757f2662cba02a6807
Daniel Rollings <danielrollings@Daniel-Rollingss-MacBookPro.local>
2016-09-19 22:14:52 +0100

    4.5 Image carousel and product partial added

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,8 @@
 class StaticPagesController < ApplicationController
     
-
   def landing_page
-  	@featured_product = Product.first
-  end
+  @products = Product.limit(3)
+	end
 
   # def index
   # end
