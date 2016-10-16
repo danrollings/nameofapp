058d51766852ef324e4ea58efa3f96138b626b16
Your Actual Namedaniel <Your Actual Emaildanielrollings@mac.com>
2016-10-16 16:46:22 +0100

    Rating system (Raty Plugin)

---
+++ app/assets/javascripts/site.js
@@ -0,0 +1,13 @@
+$(document).on('turbolinks:load', function(){
+    $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
+    $('.rated').raty({ path: '/assets',
+      readOnly: true,
+      score: function() {
+        return $(this).attr('data-score');
+      }
+    });
+});
+
+$(document).on('turbolinks:load', function(){
+	$("#alert").delay(4000).fadeOut(2000);
+});
