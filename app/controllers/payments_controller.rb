9b910d4ce03ae68e0fc6220c859e135efa150443
Your Actual Namedaniel <Your Actual Emaildanielrollings@mac.com>
2016-11-02 11:12:12 +0000

    changes to product url to allow _stripe_checkout_button.html.erb to bring through product images successfully

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,11 +1,13 @@
 class PaymentsController < ApplicationController
 
 	def create
+      @product = Product.find(params[:product_id])
+      @user = current_user
   		token = params[:stripeToken]
   		# Create the charge on Stripe's servers - this will charge the user's card
   		begin
     		charge = Stripe::Charge.create(
-      		:amount => @product.price.to_i*100, # amount in cents, again
+      		:amount => @product.price*100, # amount in cents, again
       		:currency => "gbp",
       		:source => token,
       		:description => params[:stripeEmail]
