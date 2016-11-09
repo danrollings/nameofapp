50831742420e839ce55f719fea70c76b481c465f
Your Actual Namedaniel <Your Actual Emaildanielrollings@mac.com>
2016-11-09 20:31:07 +0000

    6.14 Autnenticated ActionCable conections, comments displayed via product channel to other users, created job for broadcasting comments

--- app/assets/javascripts/channels/product.js
+++ app/assets/javascripts/channels/product.js
@@ -7,8 +7,22 @@ App.product = App.cable.subscriptions.create("ProductChannel", {
     // Called when the subscription has been terminated by the server
   },
 
- received: function() {
+	received: function(data) {
   // Called when there's incoming data on the websocket for this channel
-  $(".alert.alert-info").show().delay(4000).fadeOut(2000);
-}
+  $(".alert.alert-info").show().delay(5000).slideUp(1000)
+  $('.product-reviews').prepend(data.comment);
+  $("#average-rating").attr('data-score', data.average_rating);
+  refreshRating();
+	},
+
+	listen_to_comments: function() {
+  return this.perform('listen', {
+    product_id: $("[data-product-id]").data("product-id")
+  	});
+	}
+
+});
+
+$(document).on('turbolinks:load', function() {
+  App.product.listen_to_comments();
 });
